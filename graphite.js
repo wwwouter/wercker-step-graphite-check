7e180057b516aa5f97af0ecf3c2c8fa7e127ce77
wwwouter <wouter@wercker.com>
2013-07-08 18:47:13 +0200

    first

---
+++ graphite.js
@@ -0,0 +1,164 @@
+var underscore = require('underscore');
+var request = require('request');
+
+exports.getMeasurements = function(baseUrl, deployTargetKey, from, to, format, callback){
+    'use strict';
+    var fromSeconds = parseInt( from / 1000, 10);
+    var toSeconds = parseInt( to / 1000, 10);
+    var url = baseUrl + '/render?target=' + deployTargetKey + '&format=' + format + '&' + 'from=' + fromSeconds + '&to=' + toSeconds;
+
+    console.log(url);
+
+    request({
+            url:url,
+            method:"GET"
+        }, function (err, res, body) {
+            callback(err, body);
+        }
+    );
+};
+
+exports.getMeasurementsByNames = function(baseUrl, names, from, to, format, callback){
+    'use strict';
+
+    var fromSeconds = parseInt( from/1000, 10);
+    var toSeconds = parseInt( to/1000, 10);
+
+    var url =baseUrl + '/render?format=' + format + '&from=' + fromSeconds + '&to=' + toSeconds;
+
+    underscore.each(names, function(name){
+        url += '&target=' + name;
+    });
+
+    console.log(url);
+
+    request({
+            url:url,
+            method:"GET"
+        }, function (err, res, body) {
+            callback(err, body);
+        }
+    );
+};
+
+
+exports.rawToAverage = function(raw, ignoreWhenAllDataPointsAreNone){
+    'use strict';
+    var result = {};
+    if (!raw){
+        return result;
+    }
+    try{
+        var lines = raw.split('\n');
+        underscore.each(lines, function(line){
+            if(!!line){
+                var lineData = line.split('|');
+                var name = lineData[0].split(',')[0];
+                var dataPoints = lineData[1].split(',');
+                var allDataPointsNone = true;
+                var sum = 0;
+
+                underscore.each(dataPoints, function(dataPoint){
+                    if(dataPoint !== 'None'){
+                        allDataPointsNone = false;
+                        sum += parseFloat(dataPoint);
+                    }
+                });
+                if(!(allDataPointsNone && ignoreWhenAllDataPointsAreNone)){
+                    var average = sum / dataPoints.length;
+                    result[name] = average;
+                }
+            }
+        });
+        return result;
+    }
+    catch(error){
+        console.log(error);
+        return [];
+    }
+};
+
+
+exports.averagesToPercentageDiff = function(preAverages, postAverages){
+    'use strict';
+    var result = {};
+
+    underscore.each(preAverages, function(value, key){
+        if(postAverages[key] !== undefined){
+            if(preAverages[key] === postAverages[key] ){
+                result[key] = 0;
+            }
+            else if(preAverages[key] === 0 &&  postAverages[key] === 0){
+                result[key] = 0;
+            }
+            else if(preAverages[key] === 0 ||  postAverages[key] === 0){
+                result[key] = 1;
+            }
+            else{
+                result[key] = Math.abs(preAverages[key] - postAverages[key]) / preAverages[key];
+            }
+        }
+    });
+
+    return result;
+};
+
+exports.getNamesAboveThreshold = function(diffAverages, threshold){
+    'use strict';
+    var result = [];
+
+    underscore.each(diffAverages, function(value, key){
+        if(value > threshold){
+            result.push(key);
+        }
+    });
+
+    return result;
+
+};
+
+
+exports.formatJsonForStorage = function(baseUrl, measurementsJson, from, to, deployTargetKey){
+    'use strict';
+    var measurements = JSON.parse(measurementsJson);
+    var result = [];
+    var fromSeconds = parseInt( from/1000, 10);
+    var toSeconds = parseInt( to/1000, 10);
+
+    underscore.each(measurements, function(measurement){
+        var item = {};
+        item.title = measurement.target.replace(deployTargetKey + '.' , '');
+        item.url =baseUrl + '/render?drawNullAsZero=true&lineMode=staircase&hideLegend=true&' +
+            'title=' + item.title  + '&from=' + fromSeconds + '&to=' + toSeconds +'&target=' + measurement.target;
+
+        item.datapoints = underscore.map(measurement.datapoints, function(datapoint){
+            if(!datapoint[0]){
+                return [0, datapoint[1]] ;
+            }
+            else{
+                return datapoint;
+            }
+        });
+        result.push(item);
+    });
+
+    return result;
+};
+
+exports.getSummaryImageUrl = function(baseUrl, measurementsJson, from, to, deployTargetKey){
+    'use strict';
+    var measurements = JSON.parse(measurementsJson);
+    var result = [];
+    var fromSeconds = parseInt( from/1000, 10);
+    var toSeconds = parseInt( to/1000, 10);
+
+    var url = baseUrl + '/render?drawNullAsZero=true&lineMode=staircase&hideLegend=false&' +
+           + '&from=' + fromSeconds + '&to=' + toSeconds;
+
+    underscore.each(measurements, function(measurement){
+        url +='&target=' + measurement.target;
+
+    });
+
+    return url;
+};
---
+++ main.js
@@ -0,0 +1,178 @@
+var async = require("async");
+var fs = require("fs");
+var path = require("path");
+var request = require('request');
+var underscore = require('underscore');
+var graphite = require("./graphite");
+
+function checkMetrics(url, target,preDeployCheckEnd, preDeployPeriod, postDeployWarmUp, postDeployRange, diffThreshold ){
+    'use strict';
+
+
+    var preDeployCheckStart = null;
+
+    var postDeployCheckStart = null;
+    var postDeployCheckEnd = null;
+    var preMeasurements = null;
+    var postMeasurements = null;
+
+    postDeployCheckEnd = Date.now() - 5000;
+    console.log('postDeployRange ' + postDeployRange);
+    console.log('postDeployCheckEnd ' + postDeployCheckEnd);
+    postDeployCheckStart = postDeployCheckEnd - postDeployRange + 5000;
+
+    async.waterfall([
+        function(next){
+            preDeployCheckStart = preDeployCheckEnd - preDeployPeriod;
+
+            console.log('get pre deploy measurements');
+            console.log('preDeployCheckStart ' + preDeployCheckStart);
+            console.log('preDeployCheckEnd ' + preDeployCheckEnd);
+            console.log('preDeployPeriod ' + preDeployPeriod);
+            graphite.getMeasurements(url, target, preDeployCheckStart,preDeployCheckEnd, 'raw', next );
+        },
+        function(measurements, next) {
+            console.log('rawToAverage');
+            preMeasurements = graphite.rawToAverage(measurements,true);
+            console.log('preMeasurements');
+            console.log(JSON.stringify(preMeasurements));
+
+            console.log('get post deploy measurements');
+            console.log('postDeployCheckStart ' + postDeployCheckStart);
+            console.log('postDeployCheckEnd ' + postDeployCheckEnd);
+            graphite.getMeasurements(url, target, postDeployCheckStart,postDeployCheckEnd, 'raw', next );
+        },
+        function(measurements, next) {
+            postMeasurements = graphite.rawToAverage(measurements, false);
+            console.log('postMeasurements');
+            console.log(JSON.stringify(postMeasurements));
+            var measurementsDiff = graphite.averagesToPercentageDiff(preMeasurements,postMeasurements);
+            console.log('measurementsDiff');
+            console.log(JSON.stringify(measurementsDiff));
+            var warningNames = graphite.getNamesAboveThreshold(measurementsDiff, diffThreshold);
+            console.log('warningNames');
+            console.log(JSON.stringify(warningNames));
+
+            if(warningNames.length === 0){
+                console.log("All is ok.");
+                process.exit(0);
+            }
+
+            graphite.getMeasurementsByNames(url, warningNames, preDeployCheckStart, postDeployCheckEnd, 'json', next );
+        },
+        function(measurements, next) {
+            var imageUrl = graphite.getSummaryImageUrl(url,measurements,preDeployCheckStart, postDeployCheckEnd,target);
+            console.log('imageUrl');
+            console.log(imageUrl);
+
+            var imageFilename = path.join(process.env["WERCKER_REPORT_ARTIFACTS_DIR"], 'image.jpg');
+            console.log('writing file to ' + imageFilename)
+
+            var stream = fs.createWriteStream(imageFilename);
+            request(imageUrl).pipe(stream);
+
+            stream.on("close", function () {
+              next();
+            })
+
+            stream.on("error", function(data) {
+              next({error : data});
+            });
+        }],
+        function(err) {
+            if (err) {
+                console.log('Error while checking metrics after deploy', err);
+            }
+            console.log("Something is wrong")
+        });
+}
+
+
+
+
+if(underscore.isUndefined(process.env["WERCKER_STEP_ROOT"])){
+  console.log("WERCKER_STEP_ROOT not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_MAIN_PIPELINE_STARTED"])){
+  console.log("WERCKER_MAIN_PIPELINE_STARTED not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_MAIN_PIPELINE_FINISHED"])){
+  console.log("WERCKER_MAIN_PIPELINE_FINISHED not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_GRAPHITE_CHECK_PRE_DEPLOY_PERIOD"])){
+  console.log("WERCKER_GRAPHITE_CHECK_PRE_DEPLOY_PERIOD not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_GRAPHITE_CHECK_POST_DEPLOY_WARMUP"])){
+  console.log("WERCKER_GRAPHITE_CHECK_POST_DEPLOY_WARMUP not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_GRAPHITE_CHECK_URL"])){
+  console.log("WERCKER_GRAPHITE_CHECK_URL not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_GRAPHITE_CHECK_TARGET"])){
+  console.log("WERCKER_GRAPHITE_CHECK_TARGET not set");
+  process.exit(1);
+}
+if(underscore.isUndefined(process.env["WERCKER_GRAPHITE_CHECK_DIFF_THRESHOLD"])){
+  console.log("WERCKER_GRAPHITE_CHECK_DIFF_THRESHOLD not set");
+  process.exit(1);
+}
+
+// var WERCKER_MAIN_PIPELINE_STARTED = "1373272448"
+// var WERCKER_MAIN_PIPELINE_FINISHED = "1373272448"
+
+// var WERCKER_GRAPHITE_CHECK_PRE_DEPLOY_PERIOD = "1373272448"
+// var WERCKER_GRAPHITE_CHECK_POST_DEPLOY_WARMUP = "1373272448"
+
+//2746597441
+//1373298758
+
+//1373298723000 plf
+//1373299366287 pre deploy check end
+
+var pipelineFinished = parseInt(process.env["WERCKER_MAIN_PIPELINE_FINISHED"]) * 1000;
+var pipelineStarted = parseInt(process.env["WERCKER_MAIN_PIPELINE_STARTED"]) * 1000;
+
+var preDeployPeriod = parseInt(process.env["WERCKER_GRAPHITE_CHECK_PRE_DEPLOY_PERIOD"]) * 1000;
+var postDeployWarmUp = parseInt(process.env["WERCKER_GRAPHITE_CHECK_POST_DEPLOY_WARMUP"]) * 1000;
+console.log(process.env["WERCKER_GRAPHITE_CHECK_DIFF_THRESHOLD"])
+var diffThreshold = parseFloat(process.env["WERCKER_GRAPHITE_CHECK_DIFF_THRESHOLD"]);
+
+
+var preDeployCheckEnd = pipelineStarted;
+    console.log('diffThreshold ' + diffThreshold);
+    console.log('pipelineFinished ' + pipelineFinished);
+    console.log('postDeployWarmUp ' + postDeployWarmUp);
+
+// var preDeployPeriod = pipelineFinished - pipelineStarted - postDeployWarmUp;
+
+    console.log('preDeployCheckEnd ' + preDeployCheckEnd);
+    console.log('pipelineFinished ' + pipelineFinished);
+    console.log('preDeployPeriod ' + preDeployPeriod);
+var postDeployRange =  pipelineFinished - preDeployCheckEnd - preDeployPeriod;
+
+
+
+
+var url = process.env["WERCKER_GRAPHITE_CHECK_POST_URL"];
+var target = process.env["WERCKER_GRAPHITE_CHECK_POST_TARGET"];
+
+//var preDeployCheckEnd = Math.round(Date.now()/1000)- (5 * 60 * 1000);
+
+
+checkMetrics(url, target, preDeployCheckEnd, preDeployPeriod, postDeployWarmUp, postDeployRange, diffThreshold);
+
+
+
+
+
+
+
+
+
+
